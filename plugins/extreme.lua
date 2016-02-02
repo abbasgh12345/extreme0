e14c7420670bf7462a98b4ded9529f178dee5b12
abasshacker <Abbasserver00@gmail.com>
2016-02-02 10:30:51 +0330

    Update and rename creed.lua to extreme.lua

---
+++ plugins/extreme.lua
@@ -0,0 +1,17 @@
+do
+
+function run(msg, matches)
+  return " @unkownhacker
+  end
+return {
+  description = "Says about extreme ", 
+  usage = "!extreme or extreme : Return Information about Creed hehehe",
+  patterns = {
+    "^[Aa](bbas)$",
+    "^[Ee](xtreme)$",
+    "^[!/]([Ee]xtreme)$",
+    "^[!/]([Aa]bbas)$",
+  },
+  run = run
+}
+end
